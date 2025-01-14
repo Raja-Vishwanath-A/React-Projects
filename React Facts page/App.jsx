bca0d91eb456dff7e55e5fe25507a429eac0f04a
Ampati Raja Vishwanath <62474173+Raja-Vishwanath-A@users.noreply.github.com>
2025-01-14 20:17:17 +0530

    Add files via upload

---
+++ React Facts page/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./components/Header.jsx"
+import MainContent from "./components/MainContent.jsx"
+
+
+export default function App () {
+    return(
+      <>
+        < Header />
+        < MainContent />
+      </>
+    )
+}
---
+++ React Facts page/components/Header.jsx
@@ -0,0 +1,12 @@
+export default function Header () {
+    return(
+      <>
+        <header>
+            <nav>
+              <img src="../images/react-logo.png" alt="React-Logo" />
+              <span class="title">React Facts</span>
+            </nav>
+        </header>
+      </>
+    )
+}
